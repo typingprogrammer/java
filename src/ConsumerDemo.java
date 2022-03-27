2fc4b972541dc2726cff02ee878f84aa05d8b62f
typingprogrammer <typingprogrammer@gmail.com>
2022-03-27 22:11:24 +0530

    modifying consumer

--- src/ConsumerDemo.java
+++ src/ConsumerDemo.java
@@ -6,46 +6,44 @@ import java.util.function.Supplier;
 
 public class ConsumerDemo {
     public static void main(String[] args) {
-        Consumer<Integer> displayFunction = a -> System.out.println(a);
 
+        //Consumer and Supplier
+        // Consumer<Integer>
+        // Supplier<Integer>
+
+        Consumer<Integer> displayFunction = a -> System.out.println(a);
         displayFunction.accept(10);
 
         Consumer<String> stringFunction = (a) -> {
-            //this.getClass() - this does not work
-            //throw new RuntimeException(a);
+            throw new RuntimeException(a);
+            //System.out.println(a);
         };
 
         stringFunction.accept("hello");
 
 
-        Consumer<List<Integer>> multiplyByTwo = list ->
-        {
-            for (int i = 0; i < list.size(); i++)
-                list.set(i, 2 * list.get(i));
+        Consumer<List<Integer>> multiplyListByTwo = (list) -> {
+            for(int i=0;i<list.size();i++) {
+                list.set(i, list.get(i) *2);
+            }
         };
 
-        Consumer<List<Integer> >
-                dispList = list -> list.stream().forEach(a -> System.out.print(a + " "));
+        Consumer<List<Integer>> printTheList = (list) -> System.out.println(list);
 
         List<Integer> list = new ArrayList<Integer>();
         list.add(100);
         list.add(200);
         list.add(300);
 
-        multiplyByTwo.accept(list);
+        //multiplyListByTwo.accept(list);
+        //printTheList.accept(list);
 
-        dispList.accept(list);
-
-        System.out.println();
-        multiplyByTwo.andThen(dispList).accept(list);
-
-        System.out.println();
-        BiConsumer<Integer, Integer> sumFunction = (a,b) -> System.out.println(a+b);
-        sumFunction.accept(1,2);
+        multiplyListByTwo.andThen(printTheList).accept(list);
 
         Supplier<Double> randomValue = () -> Math.random();
-
         System.out.println(randomValue.get());
 
+        BiConsumer<Integer, Integer> sumFunction = (a,b) -> System.out.println(a+b);
+        sumFunction.accept(2,3);
     }
 }
